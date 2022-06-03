7f3a9ffe942aec01b7b3d1d83548955963fe035d
Danilo <dansantosalfa@gmail.com>
2022-06-03 09:09:59 -0300

    Criando primeiro post e get de /filmes

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,32 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filmes> listaFilmes = new List<Filmes>();
+        private static int id = 1;
+        [HttpPost]
+        public void Adicionar([FromBody] Filmes addFilmes)
+        {
+            addFilmes.Codigo = id++;
+            listaFilmes.Add(addFilmes);
+            foreach (var filmes in listaFilmes)
+            {
+                Console.WriteLine(filmes);
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Filmes> Retornar()
+        {
+            return listaFilmes;
+        }
+
+    }
+}
---
+++ FilmesAPI/Models/Filmes.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filmes
+    {
+        public Filmes(int codigo, string nome, string genero)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Genero = genero;
+        }
+        [Required(ErrorMessage = "Codigo Obrigatorio")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Nome Obrigatorio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Genero Obrigatorio")]
+        public string Genero { get; set; }
+
+        public override string ToString()
+        {
+            return $"N# {Codigo} : {Nome} - Categora: {Genero}";
+        }
+
+    }
+}
