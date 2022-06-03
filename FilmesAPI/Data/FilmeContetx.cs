cddee52a08d92d75092ace1e515ce95d2d3a149e
Danilo <dansantosalfa@gmail.com>
2022-06-03 10:58:18 -0300

    Criando conexão com BD Mysql

---
+++ FilmesAPI/Data/FilmeContetx.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContetx : DbContext
+    {
+        public FilmeContetx(DbContextOptions<FilmeContetx> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
